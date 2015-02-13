74a886c9706ad7c3ce4f2c72b44950bfcc172d3e
xouxou <xouxou_coursera@hotmail.com>
2015-02-13 16:50:17 +0100

    - mise à jour couleur

--- gen/course/project/moma/R.java
+++ gen/course/project/moma/R.java
@@ -8,7 +8,22 @@
 package course.project.moma;
 
 public final class R {
+    public static final class array {
+        public static final int androidcolors=0x7f050000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int blue=0x7f040000;
+        public static final int darkblue=0x7f040005;
+        public static final int darkgreen=0x7f040007;
+        public static final int darkorange=0x7f040008;
+        public static final int darkpurple=0x7f040006;
+        public static final int darkred=0x7f040009;
+        public static final int green=0x7f040002;
+        public static final int orange=0x7f040003;
+        public static final int purple=0x7f040001;
+        public static final int red=0x7f040004;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -18,35 +33,30 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int edit_message1=0x7f080000;
-        public static final int edit_message2=0x7f080001;
-        public static final int edit_message3=0x7f080002;
-        public static final int edit_message4=0x7f080003;
-        public static final int edit_message5=0x7f080004;
+        public static final int action_settings=0x7f0a0006;
+        public static final int seekBar1=0x7f0a0005;
+        public static final int textview1=0x7f0a0000;
+        public static final int textview2=0x7f0a0001;
+        public static final int textview3=0x7f0a0002;
+        public static final int textview4=0x7f0a0003;
+        public static final int textview5=0x7f0a0004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int edit_message1=0x7f050003;
-        public static final int edit_message2=0x7f050004;
-        public static final int edit_message3=0x7f050005;
-        public static final int edit_message4=0x7f050006;
-        public static final int edit_message5=0x7f050007;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
     }
     public static final class style {
         /** 
@@ -70,10 +80,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- src/course/project/moma/MainActivity.java
+++ src/course/project/moma/MainActivity.java
@@ -1,16 +1,79 @@
 package course.project.moma;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	private TextView mTextView1 = null;
+	private TextView mTextView2 = null;
+	private TextView mTextView3 = null;
+	private TextView mTextView4 = null;
+	private TextView mTextView5 = null;
+	private SeekBar mSeekBar = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mTextView1 = (TextView) findViewById(R.id.textview1);
+		mTextView2 = (TextView) findViewById(R.id.textview2);
+		mTextView3 = (TextView) findViewById(R.id.textview3);
+		mTextView4 = (TextView) findViewById(R.id.textview4);
+		mTextView5 = (TextView) findViewById(R.id.textview5);
+		mSeekBar = (SeekBar) findViewById(R.id.seekBar1);
+		
+		int color = getResources().getColor(R.color.blue);
+		mTextView4.setBackgroundColor(color);
+		
+		color = getResources().getColor(R.color.purple);
+		mTextView1.setBackgroundColor(color);
+		
+		color = getResources().getColor(R.color.darkblue);
+		mTextView3.setBackgroundColor(color);
+		
+		color = getResources().getColor(R.color.darkorange);
+		mTextView5.setBackgroundColor(color);
+		
+		mSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+				// TODO Auto-generated method stub
+				Log.i("progress","aaa" + progress);
+				int color = color(getResources().getColor(R.color.purple), getResources().getColor(R.color.red), progress);
+				mTextView4.setBackgroundColor(color);
+				
+				color = color(getResources().getColor(R.color.red), getResources().getColor(R.color.green), progress);
+				mTextView1.setBackgroundColor(color);
+				
+				color = color(getResources().getColor(R.color.darkblue), getResources().getColor(R.color.darkpurple), progress);
+				mTextView3.setBackgroundColor(color);
+				
+				color = color(getResources().getColor(R.color.darkorange), getResources().getColor(R.color.darkblue), progress);
+				mTextView5.setBackgroundColor(color);
+			}
+		});
 	}
 
 	@Override
@@ -30,5 +93,20 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public int color(int color1, int color2, int progress) {
+		int red, green, blue; // rgb components
+		int deltaR, deltaG, deltaB; // change in rgb components
+
+		deltaR = Color.red(color2) - Color.red(color1);
+		deltaG = Color.green(color2) - Color.green(color1);
+		deltaB = Color.blue(color2) - Color.blue(color1);
+
+		red = Color.red(color1) + (deltaR * progress / 100);
+		green = Color.green(color1) + (deltaG * progress / 100);
+		blue = Color.blue(color1) + (deltaB * progress / 100);
+
+		return Color.rgb(red, green, blue);
 	}
 }
